f8faf2d890c81113c1b244b178fa0aab937a1def
surrajj20 <shah.suraj1200@gmail.com>
2020-01-08 01:47:03 +0530

    menu added to event page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+/*NAV START*/
+const nav = document.querySelector('nav');
+const main = document.querySelector('body');
+function closeNav()
+{
+    main.style = "overflow-x: hidden";
+    nav.classList.remove('nav-open');
+    setTimeout(()=>{
+        document.querySelector('nav').style="display:none;"
+    },900);
+}
+
+function openNav()
+{
+    main.style = "overflow: hidden";
+    nav.style="";
+    setTimeout(()=>{
+    nav.classList.add('nav-open');
+    },100);
+}
+/* NAV ENDS*/
+
+/*event page code start*/
+
 ;(function () {
 	
 	'use strict';
